596aa3f92cc3a2d1f3f8b1b4670709a236cc26e3
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-10-13 17:41:37 +0545

    Desgin complete, dialog form validation, double click to toggle read, delete function

--- index.js
+++ index.js
@@ -1,46 +1,82 @@
 const main = document.querySelector('main');
 const form = document.querySelector('form');
+let message = document.getElementById('message');
 
-form.hidden = true;
+// form.hidden = true;
 
 const myLibrary = [
     new Book('David Goggin', 'Cant hurt me', 345, true),
     new Book('Jordan Pietersen', '12 rule of life', 341, false),
+    new Book('Benjamin Graham', 'Intelligent Invenstor', 700, true),
+    new Book('Benjamin Graham', 'Intelligent Invenstor', 700, true),
     new Book('Benjamin Graham', 'Intelligent Invenstor', 700, true)
-    
+
 ];
 
-// loadBook();
+loadBookAndReadEvent();
 
 function loadBook() {
     let bookDetail = '';
     myLibrary.forEach((book, index) => {
-        bookDetail += `<p> ${book.author} - ${book.title} - ${book.pages}- ${book.read} - ${index}</p>
-    <button class ="${index} librarybtn" data-key = ${index}>Delete</button>
-    `
+        bookDetail += `<article class="${index} article">
+        
+        <div class ="image-info">
+        <span>${book.title[0]}</span>
+        </div>
+        <h1>${book.title} </h1>
+        <h2> ${book.author}</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et autem iste cupiditate quam architecto
+            officia id libero. Amet nihil facilis accusantium voluptate commodi?</p>
+        <section>
+            <p>Pages : ${book.pages}</p>
+            <p>Completed : ${book.read}</p>
+        </section>
+
+        <button class="${index} deletebtn" data-key=${index}>Delete</button>
+    </article>`
     })
 
     main.innerHTML = bookDetail;
+    enableDeleteBtns();
 
 }
 
-var create = document.querySelector('#create');
-create.addEventListener('click' ,toggleForm )
 
-var btns = document.querySelectorAll('.librarybtn');
+function enableDeleteBtns() {
+    var deleteBtns = document.querySelectorAll('.deletebtn');
 
-Array.from(btns).forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        console.log(e.dataset);
+    Array.from(deleteBtns).forEach(btn => {
+        btn.addEventListener('click', deleteBook);
     })
-})
+}
+
+
+function enableToggleRead() {
+    var articles = document.querySelectorAll('.article');
+
+    Array.from(articles).forEach(btn => {
+        btn.addEventListener('dblclick', toggleRead);
+    })
+    
+}
 
 var submitbtn = document.querySelector('#submit');
-
 submitbtn.addEventListener('click', addBookToLibrary);
 
 
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("#create");
+const closeButton = document.querySelector("#close");
 
+showButton.addEventListener("click", () => {
+    message.innerHTML = '';
+    dialog.showModal();
+  });
+
+  // "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+    dialog.close();
+  });
 
 function Book(author, title, pages, read) {
     this.author = author;
@@ -49,28 +85,69 @@ function Book(author, title, pages, read) {
     this.read = read;
 }
 
+
 function addBookToLibrary() {
-    console.log(form.read)
-    form.read.value = form.read.checked ? true :false;
-    myLibrary.push(new Book(form.author.value, form.title.value, 
+    if(!validateForm()){
+        return;
+    }
+    form.read.value = form.read.checked ? true : false;
+    myLibrary.push(new Book(form.author.value, form.title.value,
         form.pages.value, form.read.value));
     form.reset();
-    loadBook();
-    toggleForm();
+    dialog.close();
+    loadBookAndReadEvent();
     return false;
-    // do stuff here
 }
 
-function deleteBook(index) {
+function deleteBook(e) {
+    let index = e.target.dataset.key;
     myLibrary.splice(index, 1);
-    loadBook();
+    loadBookAndReadEvent();
 }
 
-function toggleForm() {
-    if(form.hidden){
-        form.hidden = false;
-    } 
-    else{
-        form.hidden = true;
+function toggleRead(e) {
+    let index = e.currentTarget.classList[0];
+    myLibrary[index].read =  !myLibrary[index].read;
+    loadBookAndReadEvent();
+}
+
+
+function loadBookAndReadEvent() {
+    loadBook();
+    enableToggleRead();
+}
+
+
+
+function ErrorMessage(value, name){
+    this.value = value,
+    this.name = name;
+}
+
+ErrorMessage.prototype.returnObj = function () {
+    return {value : this.value, name :  this.name}
+}
+function validateForm(){
+    // myLibrary.push(new Book(form.author.value, form.title.value,
+    //     form.pages.value, form.read.value));
+    let inputs = [];
+    let author = new ErrorMessage(form.author.value, 'Author');
+    inputs.push(author.returnObj())
+    let title = new ErrorMessage(form.title.value, 'Title');
+    inputs.push(title.returnObj())
+    let pages = new ErrorMessage(form.pages.value, 'Pages');
+    inputs.push(pages.returnObj())
+
+    let messageText = '';
+    inputs.forEach(item => {
+        if(!item.value){
+            messageText += `<p>*${item.name} is required.</p>`
+        }
+    })
+    if(messageText){
+        message.innerHTML = messageText;
+        return false;
     }
+
+    return true;
 }
